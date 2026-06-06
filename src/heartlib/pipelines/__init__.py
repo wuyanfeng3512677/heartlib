47a45bf9c921eb285dd48eb04231ea96c7109344
wuyanfeng3512677 <wuyanfeng3512677@users.noreply.github.com>
2026-06-06 04:12:07 +0000

    feat: 歌词创作技巧
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

---
+++ src/heartlib/pipelines/__init__.py
@@ -0,0 +1,10 @@
+from .lyrics_generation import LyricConfig, LyricGenerator, LyricsWritingAssistant, SongStructure, OpeningTechnique, NarrativeTechnique
+
+__all__ = [
+    "LyricConfig",
+    "LyricGenerator",
+    "LyricsWritingAssistant",
+    "SongStructure",
+    "OpeningTechnique",
+    "NarrativeTechnique"
+]
